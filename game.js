cb79402d9748dc95537bf3be438a5d7cfc485721
stevepae <stevepaemusic@gmail.com>
2021-06-02 10:55:07 -0400

    able to log the clicked patterns

--- game.js
+++ game.js
@@ -2,11 +2,24 @@ let buttonColors = ["red", "blue", "green", "yellow"];
 
 let gamePattern = [];
 
+let userClickedPattern = [];
+
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * (4 - 0) + 0);
-  console.log(randomNumber);
+
   let randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
-}
 
-console.log(nextSequence());
+  $("#" + randomChosenColor)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  let audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  audio.play();
+}
+$("div[type='button']").on("click", function (e) {
+  let userChosenColor = e.currentTarget.id;
+  userClickedPattern.push(userChosenColor);
+  console.log(userClickedPattern);
+});
