b764d62ccffe0081ae261520353695c539bf3bd2
stevepae <stevepaemusic@gmail.com>
2021-06-02 11:59:47 -0400

    updated game logic, game now plays, logs success and fail messages

--- game.js
+++ game.js
@@ -1,6 +1,25 @@
 let buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
+let started = false;
+let level = 0;
+
+$(document).keypress(function () {
+  if (!started) {
+    $("h1").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$("div[type='button']").on("click", function (e) {
+  let userChosenColor = e.currentTarget.id;
+  userClickedPattern.push(userChosenColor);
+
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+  checkAnswer(userClickedPattern.length - 1);
+});
 
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * (4 - 0) + 0);
@@ -13,13 +32,13 @@ function nextSequence() {
     .fadeOut(100)
     .fadeIn(100);
 
+  userClickedPattern = [];
+
   playSound(randomChosenColor);
+
+  level++;
+  $("h1").text("Level " + level);
 }
-$("div[type='button']").on("click", function (e) {
-  let userChosenColor = e.currentTarget.id;
-  userClickedPattern.push(userChosenColor);
-  animatePress(userChosenColor);
-});
 
 function playSound(name) {
   let audio = new Audio("sounds/" + name + ".mp3");
@@ -32,4 +51,17 @@ function animatePress(currentColor) {
   setTimeout(function () {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+  }
+  if (userClickedPattern.length === gamePattern.length) {
+    setTimeout(function () {
+      nextSequence();
+    }, 1000);
+  } else {
+    console.log("fail");
+  }
 }
